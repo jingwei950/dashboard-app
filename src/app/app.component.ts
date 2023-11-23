e6df072fb67d00f29520019a5ce0d3e7065cabbf
Jing Wei <jingwei950@hotmail.sg>
2023-11-23 18:25:53 +0800

    feat(workspace): Initialize project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { MainComponent } from './core/main/main.component';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  imports: [MainComponent],
 })
 export class AppComponent {
   title = 'dashboard-app';
